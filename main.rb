fdbb4a6616d733eecc7add686cad5501566d786b
Matthew Dawson <matthewdawson@outlook.com>
2018-01-30 22:55:33 +1300

    initial commit

---
+++ main.rb
@@ -0,0 +1,9 @@
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"]
+#quote = "Ride the bus - John 3:10"
+#print alphabet
+guesses = []
+puts "Enter your guess mate: "
+guess = gets.chomp
+#guesses.add(guess)
+puts "Wow... "+guess+".. really?"
+puts "Come up with something OG... so sad..."
