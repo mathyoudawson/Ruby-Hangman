eebaf1ebb4e16f617eb5bd1e358881256469f98a
Matthew Dawson <matthewdawson@outlook.com>
2018-02-03 22:23:52 +1300

    Adds correct guesses to array and improve logic

--- hang.rb
+++ hang.rb
@@ -15,37 +15,63 @@ class GuessGame
     WORDS.first # TODO: make rando
   end
 
-  def set_guess_array (length, arr) #
-    puts length
-    puts arr
+  def convert_hidden_word(word)
+    i = word.size
+    loop do
+      if i == 0
+        break
+      end
+      @guess_array.push("_")
+      i -= 1
+    end
 
   end
+
+#  def set_guess_array (length, arr) #
+  #  puts length
+  #  puts arr
+
+  #end
 
   def play_game
     @lives = INITIAL_LIVES
     @guess_array = []
-
-
-    puts "u redy?"
+    puts "Games is intializing..."
+    sleep 0.5
+    puts "27%"
+    sleep 0.27
+    puts "66%"
+    sleep 0.42
+    puts "100% Game Loaded"
     hidden_word = set_hidden_word #grabs my pseudo rando word
-    set_guess_array(hidden_word.size, @guess_array) #set array with underscore that shares same length as word i am guessing :p
+    convert_hidden_word(hidden_word)
+    #set_guess_array(hidden_word.size, @guess_array) #set array with underscore that shares same length as word i am guessing :p
 
     loop do
-      puts "enter a char pl0x"
-      char = gets.chomp
-      puts "u haff #{@lives}"
+      puts "Enter a guess: "
+      char = gets.chomp.downcase
+      puts "You have #{@lives} lives left"
 
       if hidden_word.include?(char)
-        puts "u win crt + c"
-      #  @guess.push char
+        index = hidden_word.index(char)
+        @guess_array[index] = char
+        scan_array = hidden_word.scan(char) #returns array of all occurances of char in hidden_word
+        scan_counter = scan_array.size
+
+        puts "Correct guess: #{char}"
+        puts "Current status #{@guess_array.join("")}"
         #puts @guess
       else
-        puts "u bad"
+        puts "Incorrect guess: #{char}"
         lose_life
         if @lives < 1
-          puts "srry u succ"
+          puts "Bruh you dead... you only got to #{@guess_array.join("")}"
           break
         end
+      end
+      if @guess_array.include?("_") == false
+        puts "You win!"
+        puts "Word was #{hidden_word}"
       end
     end
   end
