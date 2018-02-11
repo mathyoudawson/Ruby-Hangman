c9b5d4958536da849fb5a5960da43608c72c4ab7
Matthew Dawson <matt.dawson@fluxfederation.com>
2018-02-12 10:35:14 +1300

    Added functionality to correct and incorrect guesses. First stable release

--- hangman_v2.rb
+++ hangman_v2.rb
@@ -8,12 +8,7 @@ class Game
   end
 
   def display_lives(lives) #outputs how many lives the user has left
-     puts "You have #{lives} left"
-  end
-
-  def get_input #Asks user for input and returns/assigns user_input instance variable
-     puts "Enter your guess"
-      @user_input = gets.chomp.downcase
+     "You have #{lives} lives left"
   end
 
   def initialize(lives) #Initiral method called new Game is created
@@ -27,42 +22,19 @@ class Game
       @target_word = generate_random_word
   end
 
-  def validate_user_input(input)
-      puts input
-      puts "Please enter a single character" unless input.match?(/^[a-z]$/)
-  end
 
-  def game_condition(status_update)
-      @game_status = status_update
-  end
 
   def process_correct_guess(input)
+     char_instances = (0 ... @target_word.length).find_all { |i| @target_word[i,1] == input }
+     char_instances.each do |index|
+      @guess_array[index] = input
+     end
+     puts @guess_array.join
   end
 
   def process_incorrect_guess(input)
-  end
-
-  def check_user_input(input) #takes users input and checks wether it is included in target_word
-    if @target_word.include?(input)
-      process_correct_guess(input)
-    else
-      process_incorrect_guess(input)
-    end
-  end
-
-  def check_lives
-    if(@lives < 1)
-      game_condition("lose")
-    end
-  end
-
-  def play_game
-    while @game_status == "active"
-      get_input
-      validate_user_input(@user_input)
-      check_user_input(@user_input)
-      check_lives
-    end
+     @lives -= 1
+     puts "Incorrect guess. #{display_lives(@lives)}"
   end
 
   def generate_random_word
@@ -76,6 +48,46 @@ class Game
         select_word_size -= 1
       end
       guess_array
+   end
+
+   def game_condition(status_update)
+       @game_status = status_update
+   end
+
+   def check_condition
+     if(@lives < 1) #lose condition
+       game_condition("lose")
+     end
+     if(@guess_array.join == @target_word) #win condition
+       game_condition("win")
+     end
+   end
+
+   def check_user_input(input) #takes users input and checks wether it is included in target_word
+     if @target_word.include?(input)
+       process_correct_guess(input)
+     else
+       process_incorrect_guess(input)
+     end
+   end
+
+   def validate_user_input(input)
+       puts "Please enter a single character" unless input.match?(/^[a-z]$/)
+   end
+
+   def get_input #Asks user for input and returns/assigns user_input instance variable
+     puts "Enter your guess"
+     @user_input = gets.chomp.downcase
+   end
+
+   def play_game
+     while @game_status == "active"
+      get_input #sets @user_input as what user enters in console
+      validate_user_input(@user_input) #ensure guess is a single character and prompts user to enter guess again if guess is illegal
+      check_user_input(@user_input) #compares legal input to word that is being guessed
+      check_condition #checks that game condition is still active
+     end
+     puts "GAMEOVER. You #{@game_status}"
    end
 end
 
