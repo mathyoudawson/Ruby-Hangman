23ece934f4de62cdb7c0ff6fa0202808d82006b5
Matthew Dawson <matthewdawson@outlook.com>
2018-02-11 22:34:58 +1300

    Made one class and migrated over methods

--- hang.rb
+++ hang.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+
 require 'colorize'
 
 class GuessGame
--- hangman_v2.rb
+++ hangman_v2.rb
@@ -2,58 +2,76 @@
 
 class Game
 
-   def initialize(lives)
+  def initialize_display(initial_lives) #Following initialization outputs these messages
+     puts "Welcome to hangman"
+     puts "You start with #{initial_lives} lives"
+  end
+
+  def display_lives(lives) #outputs how many lives the user has left
+     puts "You have #{lives} left"
+  end
+
+  def get_input #Asks user for input and returns/assigns user_input instance variable
+     puts "Enter your guess"
+      @user_input = gets.chomp.downcase
+  end
+
+  def initialize(lives) #Initiral method called new Game is created
       @lives = lives
-      @instance_view = View.new
-      @instance_view.initialize_display(@lives)
-      @instance_word = Word.new
+      initialize_display(@lives)
       game_condition("active")
-      @guess_array = @instance_word.generate_guess_array(select_word.size)
-      puts @guess_array
-   end
+      @guess_array = generate_guess_array(select_word.size)
+  end
 
-   def select_word
-      @target_word = @instance_word.generate_random_word
-   end
+  def select_word
+      @target_word = generate_random_word
+  end
 
-   def get_input
-      @user_input = @instance_view.get_input
-   end
+  def validate_user_input(input)
+      puts input
+      puts "Please enter a single character" unless input.match?(/^[a-z]$/)
+  end
 
-   def validate_user_input(input)
-      #if input.match?(/^[a-z]$/)
-         #input.size > 1 || input.size < 0
-         puts input
-         puts "Please enter a single character" unless input.match?(/^[a-z]$/)
-      #end
-   end
-
-   def game_condition(status_update)
+  def game_condition(status_update)
       @game_status = status_update
-   end
+  end
 
+  def process_correct_guess(input)
+  end
 
-      def play_game
-         while @game_status == "active"
-            get_input
-            #puts @user_input
-            validate_user_input(@user_input)
-         end
-      end
-end
+  def process_incorrect_guess(input)
+  end
 
-class Word
+  def check_user_input(input) #takes users input and checks wether it is included in target_word
+    if @target_word.include?(input)
+      process_correct_guess(input)
+    else
+      process_incorrect_guess(input)
+    end
+  end
 
-   def generate_random_word
+  def check_lives
+    if(@lives < 1)
+      game_condition("lose")
+    end
+  end
+
+  def play_game
+    while @game_status == "active"
+      get_input
+      validate_user_input(@user_input)
+      check_user_input(@user_input)
+      check_lives
+    end
+  end
+
+  def generate_random_word
       ["aardvark", "laptop", "badger", "elephant", "populate", "tomato", "potato"].sample
-   end
+  end
 
    def generate_guess_array(select_word_size)
-      guess_array = []
-      loop do
-        if select_word_size == 0
-          break
-        end
+     guess_array = []
+      while select_word_size > 0
         guess_array.push("_")
         select_word_size -= 1
       end
@@ -61,39 +79,5 @@ class Word
    end
 end
 
-class View
-
-   def initialize_display(initial_lives)
-      puts "Welcome to hangman"
-      puts "You start with #{initial_lives} lives"
-   end
-
-   def display_lives(lives)
-      puts "You have #{lives} left"
-   end
-   def get_input
-      puts "Enter your guess"
-      gets.chomp.downcase
-   end
-end
-
-
-
-
-# class Game
-#    CONST
-#    intitiate
-#    def
-#    def validations; end
-#    def input_options; end # TODO: TerminalPrompt and stuff
-#    def user_input; end
-# end
-
 game_instance = Game.new(5)
 game_instance.play_game
-# player = Start.new(game_instance)
-#
-# game_instance.welcome
-# game_instance.start
-#
-# #instance.select_word
